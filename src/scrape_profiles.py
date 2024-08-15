ed57716ee551eb1ccbe4ae9b7e4fb9d328e85e7d
Eli Plotkin <eliplot@umich.edu>
2024-08-15 10:10:58 -0400

    delete unnecessary imports

--- src/scrape_profiles.py
+++ src/scrape_profiles.py
@@ -1,14 +1,10 @@
 import csv
-import openpyxl
-from openpyxl.styles import Border, Side
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 from bs4 import BeautifulSoup
-import re
 
 class scrape_profiles():
     def __init__(self):
